b9273581d972094ce4cb665fdd9cee276dd350a0
Carlos Medina <CARLOSCMEDINA@YAHOO.COM>
2023-01-14 16:14:47 -0600

    Add files via upload

---
+++ TheBasic13.js
@@ -0,0 +1,91 @@
+// function print1_255() {
+//     var arr = [];
+//     for(var i = 1; i < 256; i++){
+//         arr.push(i);     
+//     }
+//     console.log(arr);
+// }
+// print1_255();
+
+// function printOdd1_225(){
+//     var sum =0;
+//     for(var i = 1; i <= 255; i++){
+//         if(i % 2 === 1){
+//             sum = sum + i;
+//         }
+//     }
+//     console.log(sum);
+    
+// }
+// printOdd1_225();
+
+// function printIntsAndSum0_255(){
+//     var sumSoFar = 0
+//     for(i= 0; i <256; i++){
+//         sumSoFar += i;
+//         console.log(i, sumSoFar);
+//     }
+// }
+
+// printIntsAndSum0_255();
+
+// function iteratAndPrintArray(arr){
+//     var sum = 0;
+//     for(var i =0; i < arr.length; i++){
+//     sum = sum + arr[i];
+//     }
+//     console.log(sum);
+// }
+// iteratAndPrintArray([3,7,8,9,10]);
+
+// function findAndPrintMax(arr){
+//     var max = arr[0];
+//     for(var i = 0; i < arr.length; i++){
+//         if(max < arr[i]) {
+//             max = arr[i]
+//         }
+//     }
+//     console.log(max);
+// }
+// findAndPrintMax([4,5,6,15,2,8]);
+
+// function getAndPrintAverage(arr){
+//     var sum = 0;
+//     for(var i = 0; i < arr.length; i++){
+//         sum = sum + arr[i]
+//     }
+//     console.log(sum / arr.length);
+// }
+// getAndPrintAverage([3,5,6,7,8]);
+
+// function returnOddsArray1To255(){
+//     var arr = [];
+//     for(var i = 1; i < 256; i++){
+//         if( i % 2 !== 0){
+//             arr.push(i);
+//         }
+//     }
+//     console.log(arr);
+// }
+// returnOddsArray1To255();
+
+// function squareArrayVals(arr){
+// for( var i = 0; i < arr.length; i++){
+//         arr[i] = arr[i] * arr[i];
+//     }
+//     console.log(arr);
+
+// }
+// squareArrayVals([2,4,5,6,7,8]);
+
+// function returnArrayCountGreaterThanY(arr,y){
+//     var count= 0;
+//     for (var i = 0; i < arr.length; i++){
+//         if(arr[i] > y){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+// returnArrayCountGreaterThanY([9, 35, 2, 3], 10);
+
